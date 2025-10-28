a9201c4438bbd9707eebc0166b8068d34ce7b05a
Uchedivine <uchedivine65@gmail.com>
2025-10-28 06:08:12 +0100

    Add fresh migrate endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/fresh-migrate', function () {
+    try {
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return response()->json([
+            'success' => true,
+            'output' => Artisan::output()
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'error' => $e->getMessage()
+        ], 500);
+    }
 });
