683722d651137146d83899fbe89d0dc858ba1fec
Uchedivine <uchedivine65@gmail.com>
2025-10-28 01:06:30 +0100

    Add temporary migration endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CountryController;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Temporary migration endpoint
 Route::get('/run-migrations', function () {
     try {
         Artisan::call('migrate', ['--force' => true]);
@@ -16,3 +23,11 @@ Route::get('/run-migrations', function () {
         ], 500);
     }
 });
+
+// Country API Routes (without /api prefix)
+Route::post('/countries/refresh', [CountryController::class, 'refresh']);
+Route::get('/countries', [CountryController::class, 'index']);
+Route::get('/countries/image', [CountryController::class, 'image']);
+Route::get('/countries/{name}', [CountryController::class, 'show']);
+Route::delete('/countries/{name}', [CountryController::class, 'destroy']);
+Route::get('/status', [CountryController::class, 'status']);
